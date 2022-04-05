3916a9b453a8e1ecb2de4f1d24b0019bea49d342
iclair-filho <73564384+iclair-filho@users.noreply.github.com>
2022-04-05 18:40:45 -0300

    styles - alterado

--- view/footer.php
+++ view/footer.php
@@ -30,20 +30,28 @@
 
 
 <script>
-  let arrow = document.querySelectorAll(".arrow");
-  for (var i = 0; i < arrow.length; i++) {
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
     arrow[i].addEventListener("click", (e) => {
-      let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
-      arrowParent.classList.toggle("showMenu");
+        let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+        arrowParent.classList.toggle("showMenu");
     });
-  }
-  let sidebar = document.querySelector(".sidebar");
-  let sidebarBtn = document.querySelector(".bx-menu");
-  // console.log(sidebarBtn);
-  sidebarBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-  });
+}
 
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+// console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+
+let tamanhoTela = window.innerWidth;
+if (tamanhoTela <= 800) {
+    var sidebarResponsivo = document.querySelector(".sidebar"); // Procurando o elemento que você quer alterar
+    sidebarResponsivo.classList.add("close"); 
+} else {
+    sidebarResponsivo.classList.remove("close");
+}
 </script>
 </body>
 

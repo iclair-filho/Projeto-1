2ae7e871bcf37c32be2a968e6765ff02dc60bfc2
iclair-filho <73564384+iclair-filho@users.noreply.github.com>
2022-04-08 21:03:44 -0300

    construçao da daschboard

--- view/dashboard.php
+++ view/dashboard.php
@@ -2,6 +2,84 @@
 include 'header.php';
 ?>
 <div class="conteudo">
+    <div class="row g-4 mt-2">
+
+        <div class="col-12 col-sm-6 col-md-3">
+            <a href="#">
+                <div class="card">
+                    <div class="img-card">
+                        <i class='bx bxs-user'></i>
+                    </div>
+
+                    <div class="card-body bg-transparent mt-0">
+                        <h4 class="text-center text-secondary">Usuários</h4>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <a href="#">
+                <div class="card">
+                    <div class="img-card">
+                        <i class='bx bxs-share-alt'></i>
+                    </div>
+
+                    <div class="card-body bg-transparent mt-0">
+                        <h4 class="text-center text-secondary">Setores</h4>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <a href="#">
+                <div class="card">
+                    <div class="img-card">
+                        <i class='bx bxs-school'></i>
+                    </div>
+
+                    <div class="card-body bg-transparent mt-0">
+                        <h4 class="text-center text-secondary">Escola</h4>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <a href="visita.php">
+                <div class="card">
+                    <div class="img-card">
+                        <i class='bx bxs-map-pin'></i>
+                    </div>
+
+                    <div class="card-body bg-transparent mt-0">
+                        <h4 class="text-center text-secondary">Visitas</h4>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <a href="#">
+                <div class="card">
+                    <div class="img-card">
+                    <i class='bx bxs-file-pdf'></i>
+                    </div>
+
+                    <div class="card-body bg-transparent mt-0">
+                        <h4 class="text-center text-secondary">Relatórios</h4>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+
+
+
+
+
+    </div>
 
 
 
